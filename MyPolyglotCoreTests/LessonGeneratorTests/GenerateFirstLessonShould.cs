b37fa7b51fb85f152e248d372a923ef9ef3f7f5c
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-06-30 09:48:49 +0300

    add simple test to generator

---
+++ MyPolyglotCoreTests/LessonGeneratorTests/GenerateFirstLessonShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using MyPolyglotCore;
+using Xunit;
+
+namespace MyPolyglotCoreTests.LessonGeneratorTests
+{
+    public class GenerateFirstLessonShould
+    {
+        [Fact]
+        public void ReturnCorrectCountOfExercises()
+        {
+            var generator = new LessonGenerator();
+
+            var lesson = generator.GenerateFirstLesson();
+
+            Assert.True(lesson.Exercises.ToList().Count == LessonGenerator.NumberOfExerciseInLesson);
+        }
+    }
+}

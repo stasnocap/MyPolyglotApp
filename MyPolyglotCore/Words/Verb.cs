760d5b915469439707e2df00494f7dfd66b9fda3
kss <kss@37_414_3>
2021-07-26 14:15:27 +0300

    add generating present participle form of verb

--- MyPolyglotCore/Words/Verb.cs
+++ MyPolyglotCore/Words/Verb.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +7,56 @@ namespace MyPolyglotCore.Words
 {
     public class Verb : Word
     {
-        public string PastTense { get; set; }
-        public string PastParticle { get; set; }
+        public string PastForm { get; set; } //2nd form
+        public string PastParticipleForm { get; set; } // 3rd form
+        public string PresentParticipleForm { get; } // ing
+        public string ThirdPersonForm { get; } // s
+        public IReadOnlyCollection<string> AdditionalForms { get; set; }
+        public bool StressOnTheFinalSyllable { get; set; } 
+
+        private const string IngEnding = "ing";
+        private IReadOnlyCollection<char> _consonants;
+        private IReadOnlyCollection<char> _vowels;
+
+        public Verb() { } // remove
+
+        public Verb(string text, IReadOnlyCollection<char> consonants, IReadOnlyCollection<char> vowels, bool stressOnTheFinalSyllable = false)
+        {
+            Text = text;
+            StressOnTheFinalSyllable = stressOnTheFinalSyllable;
+            _consonants = consonants;
+            _vowels = vowels;
+            PresentParticipleForm = GeneratePresentParticipleForm();
+        }
 
         public bool IsIrregularVerb(IEnumerable<Verb> vocabulary)
         {
             return vocabulary.FirstOrDefault(x => x.Text == Text) != null;
+        }
+
+        private string GeneratePresentParticipleForm()
+        {
+            var lengthOfText = Text.Length;
+            var lastTwoChars = Text.Substring(lengthOfText - 2);
+            var preLastChar = lastTwoChars[0];
+            var lastChar = lastTwoChars[1];
+
+            if (StressOnTheFinalSyllable && _vowels.Contains(preLastChar) && _consonants.Contains(lastChar))
+            {
+                return Text + lastChar + IngEnding;
+            }
+
+            if (preLastChar == 'i' && lastChar == 'e')
+            {
+                return Text.Substring(0, lengthOfText - 2) + 'y' + IngEnding;
+            }
+
+            if (lastChar == 'e')
+            {
+                return Text.Substring(0, lengthOfText - 1) + IngEnding;
+            }
+
+            return Text + IngEnding;
         }
     }
 }
---
+++ MyPolyglotCoreTests/VerbTests/GeneratePresentParticipleFormShould.cs
@@ -0,0 +1,62 @@
+﻿using MyPolyglotCore;
+using MyPolyglotCore.Words;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace MyPolyglotCoreTests.VerbTests
+{
+    public class GeneratePresentParticipleFormShould
+    {
+        private Vocabulary _vocabulary;
+        private HashSet<char> _consonants;
+        private HashSet<char> _vowels;
+
+        public GeneratePresentParticipleFormShould()
+        {
+            _vocabulary = new Vocabulary();
+            _consonants = typeof(Vocabulary).GetField("_consonants", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(_vocabulary) as HashSet<char>;
+            _vowels = typeof(Vocabulary).GetField("_vowels", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(_vocabulary) as HashSet<char>;
+        }
+
+        [Theory]
+        [InlineData("hit", "hitting", true)]
+        [InlineData("begin", "beginning", true)]
+        [InlineData("listen", "listening", false)]
+        [InlineData("follow", "following", false)]
+        public void TransformVowelPlusConsonantToDoubleConsonantPlusIng(string text, string expectedText, bool stressOnTheFinalSyllable)
+        {
+            CheckIfVerbGeneratesWithRightWord(text, expectedText, stressOnTheFinalSyllable);
+        }
+
+        [Theory]
+        [InlineData("bake", "baking")]
+        [InlineData("eliminate", "eliminating")]
+        public void TransformEToRemoveEPlusIng(string text, string expectedText)
+        {
+            CheckIfVerbGeneratesWithRightWord(text, expectedText);
+        }
+
+        [Theory]
+        [InlineData("die", "dying")]
+        [InlineData("lie", "lying")]
+        public void TransformIEtoYING(string text, string expectedText)
+        {
+            CheckIfVerbGeneratesWithRightWord(text, expectedText);
+        }
+
+        [Theory]
+        [InlineData("watch", "watching")]
+        [InlineData("discover", "discovering")]
+        public void JustAddIng(string text, string expectedText)
+        {
+            CheckIfVerbGeneratesWithRightWord(text, expectedText);
+        }
+
+        private void CheckIfVerbGeneratesWithRightWord(string text, string expectedText, bool stressOnTheFinalSyllable = false)
+        {
+            var verb = new Verb(text, _consonants, _vowels, stressOnTheFinalSyllable);
+            Assert.Equal(expectedText, verb.PresentParticipleForm);
+        }
+    }
+}

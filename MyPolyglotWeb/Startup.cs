2d9151eefda353f2b42f1f97907f859ef42e7b2a
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-07-28 21:10:47 +0300

    working

--- MyPolyglotWeb/Startup.cs
+++ MyPolyglotWeb/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyPolyglotWeb.Presentation;
+using MyPolyglotWeb.Repositories;
 
 namespace MyPolyglotWeb
 {
@@ -19,6 +21,8 @@ namespace MyPolyglotWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebContext>(
+                 options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             RegisterPresentation(services);
         }

31e3d6f3f6c959448ed74befc4bd462da42e684c
kss <kss@37_414_3>
2021-08-20 15:17:51 +0300

    workign

--- MyPolyglotWeb/wwwroot/js/addExercise.js
+++ MyPolyglotWeb/wwwroot/js/addExercise.js
@@ -3,5 +3,13 @@
 });
 
 $(document).on('change', '.unrecognized select', function () {
+    if ($(this).val() == 'Verb') {
+        //if ($(this).parent().find('input[type=text]').length == 1)
+        $(this).parent().append('<input placeholder="trstasrt" type="text" />');
+    }
+    $(this).hide();
+});
+
+$(document).on('focusout', '.unrecognized input[type=text]', function () {
     $(this).hide();
 });

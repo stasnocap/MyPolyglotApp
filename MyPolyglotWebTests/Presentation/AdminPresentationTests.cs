c64c1f1911536ece52906014200dc140a7bc2d04
kss <kss@37_414_3>
2021-08-27 09:05:32 +0300

    fix AddExercise in AdminPresentationTests

--- MyPolyglotWebTests/Presentation/AdminPresentationTests.cs
+++ MyPolyglotWebTests/Presentation/AdminPresentationTests.cs
@@ -28,13 +28,19 @@ namespace MyPolyglotWebTests.Presentation
         {
             var lessonId = 1;
             var addExerciseVMMock = new Mock<AddExerciseVM>();
-            addExerciseVMMock.Setup(x => x.LessonId).Returns(lessonId.ToString);
+            var lessonDBMock = new Mock<LessonDB>();
+            var exerciseDBMock = new Mock<ExerciseDB>();
+
+            addExerciseVMMock.Setup(x => x.LessonId).Returns(lessonId.ToString());
+            _mapperMock.Setup(x => x.Map<ExerciseDB>(addExerciseVMMock.Object)).Returns(exerciseDBMock.Object);
+            _lessonRepositoryMock.Setup(x => x.Get(lessonId)).Returns(lessonDBMock.Object);
 
             _adminPresentation.AddExercise(addExerciseVMMock.Object);
 
             _mapperMock.Verify(x => x.Map<ExerciseDB>(addExerciseVMMock.Object), Times.Once);
+            //exerciseDBMock.VerifySet(x => x.Lesson = lessonDBMock.Object);
             _lessonRepositoryMock.Verify(x => x.Get(lessonId), Times.Once);
-            _exerciseRepositoryMock.Verify(x => x.Save(It.IsAny<ExerciseDB>()), Times.Once);
+            _exerciseRepositoryMock.Verify(x => x.Save(exerciseDBMock.Object), Times.Once);
         }
     }
 }

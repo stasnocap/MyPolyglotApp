8c7a927e1dd0b9a59e8cbe7d943ddc90f5bfa5ca
kss <kss@37_414_3>
2021-08-27 08:45:09 +0300

    refactor AddExercise in AdminPresentationTests

--- MyPolyglotWebTests/Presentation/AdminPresentationTests.cs
+++ MyPolyglotWebTests/Presentation/AdminPresentationTests.cs
@@ -4,11 +4,6 @@ using MyPolyglotWeb.Models.DomainModels;
 using MyPolyglotWeb.Models.ViewModels;
 using MyPolyglotWeb.Presentation;
 using MyPolyglotWeb.Repositories.IRepository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace MyPolyglotWebTests.Presentation
@@ -28,13 +23,10 @@ namespace MyPolyglotWebTests.Presentation
             _adminPresentation = new AdminPresentation(_mapperMock.Object, _lessonRepositoryMock.Object, _exerciseRepositoryMock.Object);
         }
 
-        [Theory]
-        [InlineData(1)]
-        [InlineData(5)]
-        [InlineData(7)]
-        [InlineData(31)]
-        public void AddExercise_GivenValidLessonId(int lessonId)
+        [Fact]
+        public void AddExercise()
         {
+            var lessonId = 1;
             var addExerciseVMMock = new Mock<AddExerciseVM>();
             addExerciseVMMock.Setup(x => x.LessonId).Returns(lessonId.ToString);
 
@@ -43,23 +35,6 @@ namespace MyPolyglotWebTests.Presentation
             _mapperMock.Verify(x => x.Map<ExerciseDB>(addExerciseVMMock.Object), Times.Once);
             _lessonRepositoryMock.Verify(x => x.Get(lessonId), Times.Once);
             _exerciseRepositoryMock.Verify(x => x.Save(It.IsAny<ExerciseDB>()), Times.Once);
-        }
-
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(32)]
-        [InlineData(33)]
-        public void AddExercise_GivenInvalidLessonId(int lessonId)
-        {
-            var addExerciseVMMock = new Mock<AddExerciseVM>();
-            addExerciseVMMock.Setup(x => x.LessonId).Returns(lessonId.ToString);
-
-            _adminPresentation.AddExercise(addExerciseVMMock.Object);
-
-            _mapperMock.Verify(x => x.Map<ExerciseDB>(addExerciseVMMock.Object), Times.Never);
-            _lessonRepositoryMock.Verify(x => x.Get(lessonId), Times.Never);
-            _exerciseRepositoryMock.Verify(x => x.Save(It.IsAny<ExerciseDB>()), Times.Never);
         }
     }
 }

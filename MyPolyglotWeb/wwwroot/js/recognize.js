1e9bc248bc71312a6390c7de20aeb21b36e5dac7
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-08-29 17:19:20 +0300

    AddExercise page is refactored

--- MyPolyglotWeb/wwwroot/js/recognize.js
+++ MyPolyglotWeb/wwwroot/js/recognize.js
@@ -1,27 +1,29 @@
 ﻿$(document).ready(function () {
-    $('.recognize .button').on('click', function () {
-        var engPhrase = $('.eng-phrase input[type=text]').val().trim();
+    $('.add-exercise__recognize-btn').on('click', function () {
+        var engPhrase = $('.add-exercise__eng-phrase input[type=text]').val().trim();
         if (!engPhrase) {
             return;
         }
-        var submitButton = $('.add-exercise .submit-button')
 
         $.get(`/Admin/Recognize?engPhrase=${engPhrase}`).done(function (unrecognizedArray) {
             if (unrecognizedArray.length > 0) {
+                $('.add-exercise__unrecognized-words div').empty();
                 saveOnPage(unrecognizedArray, getUnrecognizableEnumOptions());
             }
         });
 
-        submitButton.show();
+        $('.add-exercise__unrecognized-words label').show();
+        $('.add-exercise__submit-btn').prop('disabled', false);
     });
 });
 
 function saveOnPage(unrecognizedArray, options) {
-    $('.unrecognized').css('display', 'flex');
     for (var i = 0; i < unrecognizedArray.length; i++) {
-        $('.unrecognized').append(`
-            <div>
-                <span class="button">${unrecognizedArray[i].text}</span>
+        $('.unrecognized-words-js').append(`
+            <div class="unrecognized-word">
+                <div class="unrecognized-word__header">
+                    <span>${unrecognizedArray[i].text}</span>
+                </div>
                 <select name=UnrecognizedWords[${i}].Type>${options}</select>
                 <input name="UnrecognizedWords[${i}].Text" type="hidden" value="${unrecognizedArray[i].text}"/>
             </div>`

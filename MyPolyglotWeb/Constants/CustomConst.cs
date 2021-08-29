6d74700ae6e761761deb4faee1823f4e7357be51
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-08-29 19:22:30 +0300

    take LessonsNames from hostseed and bring it to CustomConsts, add complete filling lessons on index page

--- MyPolyglotWeb/Constants/CustomConst.cs
+++ MyPolyglotWeb/Constants/CustomConst.cs
@@ -1,8 +1,45 @@
-﻿namespace MyPolyglotWeb.Constants
+﻿using System.Collections.Generic;
+
+namespace MyPolyglotWeb.Constants
 {
     public class CustomConst
     {
         public const int NumberOfLessons = 31;
+
+        public static List<string> LessonsNames = new List<string>()
+        {
+            "Базовая форма глагола",
+            "Местоимения. Вопросительные слова",
+            "Глагол to be. Предлоги места. Like/Want",
+            "Притяжательные местоимения",
+            "Профессии. Этикет",
+            "Степени сравнения прилагательных. Указательные местоимения",
+            "Слова-параметры. Употребление muck и many, little и few",
+            "Предлоги и параметры времени",
+            "There is / There are",
+            "Предлоги направления и движения",
+            "Модальные глаголы can, must, should",
+            "Continuous",
+            "Описание людей. Погода",
+            "Повелительное наклонение",
+            "Фразовые глаголы",
+            "Present Perfect",
+            "Past Perfect",
+            "Future Perfect",
+            "Perfect Tenses",
+            "Present Perfect Continuous",
+            "Past Perfect Continuous",
+            "Future Perfect Continuous",
+            "Perfect Continuous Tenses",
+            "Present Participle Simple",
+            "Past Participle",
+            "Present Participle Perfect",
+            "The Participle",
+            "Simple Passive",
+            "Continuous Passive",
+            "Perfect Passive",
+            "Passive Voice"
+        };
 
         public const string UserTriesToBreakMe = "Зачем ты пытаешься сломать меня?";
     }

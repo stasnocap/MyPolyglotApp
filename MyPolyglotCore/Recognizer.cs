c68383d2bfc345b5f029de2094e39587a16c5aed
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-09-09 06:40:12 +0300

    working

--- MyPolyglotCore/Recognizer.cs
+++ MyPolyglotCore/Recognizer.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using MyPolyglotCore.Words;
+using MyPolyglotCore.Words.Pronouns;
 
 namespace MyPolyglotCore
 {
@@ -19,8 +21,37 @@ namespace MyPolyglotCore
         public void Recognize()
         {
             var words = EngPhrase.SplitToWords();
-            RecognizedWords = words.SelectMany(word => Vocabulary.RecognizableVocabularies.Where(x => x.Equals(word)));
+            RecognizedWords = words.SelectMany(word => FindWordsInVocabulary(word));
             UnrecognizedWords = words.Where(x => !RecognizedWords.Contains(x));
+        }
+
+        private IEnumerable<Word> FindWordsInVocabulary(Word word)
+        {
+            var list = new List<Word>();
+            foreach (var wordFromVocabulary in Vocabulary.RecognizableVocabularies)
+            {
+                if (wordFromVocabulary.Equals(word))
+                {
+                    var type = wordFromVocabulary.GetType();
+                    list.Add(ConvertWord(wordFromVocabulary.GetType(), word.Text));
+                }
+            }
+            return list;
+        }
+
+        private Word ConvertWord(Type type, string text)
+        {
+
+            return type switch
+            {
+                Type subjectPronoun when subjectPronoun == typeof(SubjectPronoun) => new SubjectPronoun(text),
+                Type objectPronoun when objectPronoun == typeof(ObjectPronoun) => new ObjectPronoun(text),
+                Type possessiveAdjective when possessiveAdjective == typeof(PossessiveAdjective) => new PossessiveAdjective(text),
+                Type possessivePronoun when possessivePronoun == typeof(PossessivePronoun) => new PossessivePronoun(text),
+                Type reflexivePronoun when reflexivePronoun == typeof(ReflexivePronoun) => new ReflexivePronoun(text),
+                Type determiner when determiner == typeof(Determiner) => new Determiner(text),
+                _ => throw new NotImplementedException(),
+            };
         }
     }
 }

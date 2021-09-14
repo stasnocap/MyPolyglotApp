fcf108c654c4736c0f01d6de1397f2e89d4b1158
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-09-14 19:58:02 +0300

    add basic validation

--- MyPolyglotWeb/Models/ViewModels/RegisterUserVM.cs
+++ MyPolyglotWeb/Models/ViewModels/RegisterUserVM.cs
@@ -1,8 +1,19 @@
-﻿namespace MyPolyglotWeb.Models.ViewModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPolyglotWeb.Models.ViewModels
 {
     public class RegisterUserVM
     {
+        [DisplayName("Электронный адрес")]
+        [Required(ErrorMessage = "Вы забыли ввести электронный адрес!")]
+        [EmailAddress(ErrorMessage = "Что-то не похоже не электронные адрес :(")]
         public string Email { get; set; }
+
+        [DisplayName("Пароль")]
+        [Required(ErrorMessage = "Вы забыли ввести пароль!")]
+        [MinLength(4, ErrorMessage = "Пароль должен быть больше 4 символов.")]
+        [MaxLength(10, ErrorMessage = "Пароль не должен превышать 10 символов.")]
         public string Password { get; set; }
     }
 }

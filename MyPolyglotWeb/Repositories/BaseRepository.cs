0b133abddcf2ec1604d7edd5d02f616be3796661
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-09-16 19:48:14 +0300

    delete unused methods from base repository

--- MyPolyglotWeb/Repositories/BaseRepository.cs
+++ MyPolyglotWeb/Repositories/BaseRepository.cs
@@ -21,21 +21,6 @@ namespace MyPolyglotWeb.Models
             return _dbSet.SingleOrDefault(x => x.Id == id);
         }
 
-        public IQueryable<DbModel> GetAll()
-        {
-            return _dbSet;
-        }
-
-        public int Count()
-        {
-            return _dbSet.Count();
-        }
-
-        public virtual bool IsAny()
-        {
-            return _dbSet.Any();
-        }
-
         public virtual void Save(DbModel model)
         {
             if (model.Id > 0)
--- MyPolyglotWeb/Repositories/IRepositories/IBaseRepository.cs
+++ MyPolyglotWeb/Repositories/IRepositories/IBaseRepository.cs
@@ -5,10 +5,7 @@ namespace MyPolyglotWeb.Repositories.IRepositories
 {
     public interface IBaseRepository<DbModel> where DbModel : BaseDB
     {
-        int Count();
         DbModel Get(long id);
-        IQueryable<DbModel> GetAll();
-        bool IsAny();
         void Remove(DbModel model);
         void Remove(long id);
         void Save(DbModel model);

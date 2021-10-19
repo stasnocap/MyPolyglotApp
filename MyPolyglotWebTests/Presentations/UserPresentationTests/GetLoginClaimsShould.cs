0da382e4721a6a52bde3f47d4febe2eb8afdb839
Станислав Капитан <1100511609@bntuby.onmicrosoft.com>
2021-10-19 20:07:36 +0300

    refactor user presentation tests

---
+++ MyPolyglotWebTests/Presentations/UserPresentationTests/GetLoginClaimsShould.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Moq;
+using MyPolyglotWeb.Models.DomainModels;
+using MyPolyglotWeb.Presentations;
+using MyPolyglotWeb.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyPolyglotWebTests.Presentations.UserPresentationTests
+{
+    public class GetLoginClaimsShould
+    {
+        private UserPresentation _userPresentation;
+        private Mock<IMapper> _mapperMock;
+        private Mock<IUserRepository> _userRepository;
+
+        public GetLoginClaimsShould()
+        {
+            _mapperMock = new Mock<IMapper>();
+            _userRepository = new Mock<IUserRepository>();
+            _userPresentation = new UserPresentation(_userRepository.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public void GetUserFromRepository()
+        {
+            var email = "stas@stas.ru";
+            var password = "stas";
+            var userDB = new UserDB();
+            _userRepository.Setup(x => x.GetUser(email, password)).Returns(userDB);
+
+            var claimsPrincipal = _userPresentation.GetLoginClaims(email, password);
+
+            _userRepository.Verify(x => x.GetUser(email, password), Times.Once);
+        }
+
+        [Fact]
+        public void ContainsUserIdClaim()
+        {
+            var email = "stas@stas.ru";
+            var password = "stas";
+            var userDB = new UserDB()
+            {
+                Id = 1
+            };
+            _userRepository.Setup(x => x.GetUser(email, password)).Returns(userDB);
+
+            var claimsPrincipal = _userPresentation.GetLoginClaims(email, password);
+
+            Assert.Equal(claimsPrincipal.FindFirst("Id").Value, userDB.Id.ToString());
+        }
+
+        [Fact]
+        public void ContainsUserRoleClaim()
+        {
+            var email = "stas@stas.ru";
+            var password = "stas";
+            var userDB = new UserDB()
+            {
+                Role = UserRole.User
+            };
+            _userRepository.Setup(x => x.GetUser(email, password)).Returns(userDB);
+
+            var claimsPrincipal = _userPresentation.GetLoginClaims(email, password);
+
+            Assert.Equal(claimsPrincipal.FindFirst(ClaimTypes.Role).Value, userDB.Role.ToString());
+        }
+    }
+}
---
+++ MyPolyglotWebTests/Presentations/UserPresentationTests/GetUserShould.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Moq;
+using MyPolyglotWeb.Models.DomainModels;
+using MyPolyglotWeb.Presentations;
+using MyPolyglotWeb.Repositories.IRepositories;
+using Xunit;
+
+namespace MyPolyglotWebTests.Presentations.UserPresentationTests
+{
+    public class GetUserShould
+    {
+        private UserPresentation _userPresentation;
+        private Mock<IMapper> _mapperMock;
+        private Mock<IUserRepository> _userRepository;
+
+        public GetUserShould()
+        {
+            _mapperMock = new Mock<IMapper>();
+            _userRepository = new Mock<IUserRepository>();
+            _userPresentation = new UserPresentation(_userRepository.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public void ReturnUser_GivenRightEmailAndPassword()
+        {
+            var email = "stas@stas.ru";
+            var password = "stas";
+            var userDB = new UserDB();
+            _userRepository.Setup(x => x.GetUser(email, password)).Returns(userDB);
+
+            var user = _userPresentation.GetUser(email, password);
+
+            _userRepository.Verify(x => x.GetUser(email, password), Times.Once);
+            Assert.Equal(user, userDB);
+        }
+
+
+        [Fact]
+        public void ReturnNull_GivenWrongEmail()
+        {
+            var email = "wrongstas@stas.ru";
+            var password = "stas";
+            var userDB = (UserDB)null;
+            _userRepository.Setup(x => x.GetUser(email, password)).Returns(userDB);
+
+            var user = _userPresentation.GetUser(email, password);
+
+            _userRepository.Verify(x => x.GetUser(email, password), Times.Once);
+            Assert.Equal(user, userDB);
+        }
+
+        [Fact]
+        public void ReturnNull_GivenWrongPassword()
+        {
+            var email = "stas@stas.ru";
+            var password = "wrongstas";
+            var userDB = (UserDB)null;
+            _userRepository.Setup(x => x.GetUser(email, password)).Returns(userDB);
+
+            var user = _userPresentation.GetUser(email, password);
+
+            _userRepository.Verify(x => x.GetUser(email, password), Times.Once);
+            Assert.Equal(user, userDB);
+        }
+    }
+}

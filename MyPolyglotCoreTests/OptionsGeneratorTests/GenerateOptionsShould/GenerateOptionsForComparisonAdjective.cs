5c28afcd3acc2568394d706756b17c95b05e5448
kss <kss@37_414_3>
2021-10-29 13:31:55 +0300

    add options generator tests for comparison adjective

---
+++ MyPolyglotCoreTests/OptionsGeneratorTests/GenerateOptionsShould/GenerateOptionsForComparisonAdjective.cs
@@ -0,0 +1,33 @@
+﻿using MyPolyglotCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyPolyglotCoreTests.OptionsGeneratorTests.GenerateOptionsShould
+{
+    public class GenerateOptionsForComparisonAdjective : OptionsChecker
+    {
+        [Fact]
+        public void ReturnAllComparisonAdjectiveForms()
+        {
+            var randomComparisonAdjective = RandomWordHelper.GetRandomComparisonAdjective();
+
+            var comparisonAdjectiveForms = new string[]
+            {
+                randomComparisonAdjective.Text,
+                randomComparisonAdjective.ComparativeForm,
+                randomComparisonAdjective.SuperlativeForm,
+            };
+
+            var options = _optionsGenerator.GetOptions(randomComparisonAdjective);
+
+            foreach (var comparisonAdjectiveForm in comparisonAdjectiveForms)
+            {
+                Assert.Contains(comparisonAdjectiveForm, options);
+            }
+        }
+    }
+}

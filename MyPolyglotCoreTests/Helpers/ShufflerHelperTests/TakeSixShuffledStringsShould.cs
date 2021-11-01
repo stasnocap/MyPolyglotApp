3df4608e6dce22182e8e668872d1a2a65533a250
kss <kss@37_414_3>
2021-11-01 09:07:53 +0300

    add tests to TakeSixShuffledStrings

---
+++ MyPolyglotCoreTests/Helpers/ShufflerHelperTests/TakeSixShuffledStringsShould.cs
@@ -0,0 +1,65 @@
+﻿using MyPolyglotCore.Helpers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MyPolyglotCoreTests.Helpers.ShufflerHelperTests
+{
+    public class TakeSixShuffledStringsShould
+    {
+        [Fact]
+        public void ContainsRightWord()
+        {
+            var collection = new List<string>()
+            {
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "ten",
+            };
+
+            var rightWord = "eleven";
+
+            var shuffled = collection.TakeSixShuffledStrings(rightWord);
+
+            Assert.Contains(rightWord, shuffled);
+        }
+
+        [Fact]
+        public void ContainsOtherWordsFromCollection()
+        {
+            var collection = new List<string>()
+            {
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "ten",
+            };
+
+            var rightWordStr = "eleven";
+
+            var shuffled = collection.TakeSixShuffledStrings(rightWordStr);
+
+            foreach (var wordStr in shuffled)
+            {
+                if (wordStr == rightWordStr)
+                {
+                    continue;
+                }
+
+                Assert.Contains(wordStr, collection);
+            }
+        }
+    }
+}
